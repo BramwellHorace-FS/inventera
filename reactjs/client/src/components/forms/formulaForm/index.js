750d991540e5b53cd97d12b6bacfc3b49ed4981b
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-18 17:38:44 -0400

    Updated material form and formula form removing extra styles

--- reactjs/client/src/components/forms/formulaForm/index.js
+++ reactjs/client/src/components/forms/formulaForm/index.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import {
+  Form,
+  Button,
+  ButtonGroup,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import Styles from './formulaForm.module.css';
 
 export default function FormulaForm({ handleClose }) {
   const [validated, setValidated] = useState(false);
@@ -21,57 +25,75 @@ export default function FormulaForm({ handleClose }) {
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Form.Group>
-        <Form.Label className={Styles.label}>
-          Name
-          <Form.Control type="text" placeholder="Enter formula name" required />
-          <Form.Control.Feedback type="invalid">
-            Please enter a name for the formula.
-          </Form.Control.Feedback>
-        </Form.Label>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">Name </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter formula name"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a name for the formula.
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
 
       <Form.Group>
-        <Form.Label className={Styles.label}>
-          Container Fill (oz)
-          <Form.Control
-            type="number"
-            placeholder="Enter container fill"
-            required
-            step="0.1"
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a container fill in oz. Numbers only.
-          </Form.Control.Feedback>
-        </Form.Label>
-        <Form.Label className={Styles.label}>
-          Fragrance Load (%)
-          <Form.Control
-            type="number"
-            placeholder="Enter fragrance load"
-            required
-            step="0.1"
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a fragrance load percentage. Numbers only.
-          </Form.Control.Feedback>
-        </Form.Label>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">
+                Container Fill (oz)
+              </Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter container fill"
+                required
+                step="0.1"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a container fill in oz. Numbers only.
+              </Form.Control.Feedback>
+            </Col>
+
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">
+                Fragrance Load (%)
+              </Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter fragrance load"
+                required
+                step="0.1"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a fragrance load percentage. Numbers only.
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
 
       <Form.Group>
-        <Form.Label className={Styles.label}>
-          Notes
-          <Form.Control as="textarea" rows="3" placeholder="Enter notes" />
-        </Form.Label>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">Notes </Form.Label>
+              <Form.Control as="textarea" rows="3" placeholder="Enter notes" />
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
-      <ButtonGroup className={Styles.btnGroup}>
-        <Button
-          variant="secondary"
-          onClick={handleClose}
-          className={Styles.cancelBtn}
-        >
+
+      <ButtonGroup className="mt-3 d-flex gap-3 p-2">
+        <Button variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
-        <Button variant="primary" type="submit" className={Styles.submitBtn}>
+        <Button variant="primary" type="submit">
           Submit
         </Button>
       </ButtonGroup>
--- reactjs/client/src/components/forms/materialForm/index.js
+++ reactjs/client/src/components/forms/materialForm/index.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import {
+  Form,
+  Button,
+  ButtonGroup,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import Styles from './materialForm.module.css';
 
 export default function MaterialForm({ handleClose }) {
   const [validated, setValidated] = useState(false);
@@ -20,122 +24,173 @@ export default function MaterialForm({ handleClose }) {
   return (
     <Form onSubmit={handleSubmit} noValidate validated={validated}>
       <Form.Group>
-        <Form.Label className={Styles.labelFw}>
-          Material Name
-          <Form.Control
-            name="name"
-            type="text"
-            placeholder="Enter Material Name"
-            required
-          />
-        </Form.Label>
-      </Form.Group>
-      <Form.Group className={Styles.formGroup}>
-        <Form.Label className={Styles.label}>
-          Current Stock Level
-          <Form.Control
-            name="stockLevel"
-            type="number"
-            placeholder="Enter Current Stock Level"
-            required
-          />
-        </Form.Label>
-
-        <Form.Label className={Styles.label}>
-          Min Stock Level
-          <Form.Control
-            name="minStockLevel"
-            type="number"
-            placeholder="Enter Min Stock Level"
-            required
-          />
-        </Form.Label>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6">Material Name </Form.Label>
+              <Form.Control
+                name="name"
+                type="text"
+                placeholder="Enter Material Name"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a name.
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
 
-      <Form.Group className={Styles.formGroup}>
-        <Form.Label className={Styles.label}>
-          Unit Price
-          <Form.Control
-            name="unitPrice"
-            type="number"
-            placeholder="Enter Unit Price"
-            required
-          />
-        </Form.Label>
-
-        <Form.Label className={Styles.label}>
-          Unit Type
-          <Form.Select
-            name="unitType"
-            placeholder="Select Unit Type"
-            required
-            aria-label="Default select unit type"
-          >
-            <option>Select unit type</option>
-            <option value="kg">kg</option>
-            <option value="g">g</option>
-            <option value="l">l</option>
-            <option value="ml">ml</option>
-            <option value="piece">piece</option>
-            <option value="lb">lb</option>
-            <option value="oz">oz</option>
-          </Form.Select>
-        </Form.Label>
+      <Form.Group>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">
+                Current Stock Level
+              </Form.Label>
+              <Form.Control
+                name="stockLevel"
+                type="number"
+                placeholder="Enter Current Stock Level"
+                required
+                step="0.1"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter stock level
+              </Form.Control.Feedback>
+            </Col>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">
+                Min Stock Level
+              </Form.Label>
+              <Form.Control
+                name="minStockLevel"
+                type="number"
+                placeholder="Enter Min Stock Level"
+                required
+                step="0.1"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter min stock level
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
 
-      <Form.Group className={Styles.formGroup}>
-        <Form.Label className={Styles.label}>
-          Category
-          <Form.Control
-            name="Category"
-            type="text"
-            placeholder="Enter Category"
-            required
-          />
-        </Form.Label>
-
-        <Form.Label className={Styles.label}>
-          Supplier
-          <Form.Control
-            name="Supplier"
-            type="text"
-            placeholder="Enter Supplier"
-            required
-          />
-        </Form.Label>
+      <Form.Group>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">Unit Price</Form.Label>
+              <Form.Control
+                name="unitPrice"
+                type="number"
+                placeholder="Enter Unit Price"
+                required
+                step="0.1"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a unit price.
+              </Form.Control.Feedback>
+            </Col>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">Unit Type</Form.Label>
+              <Form.Select
+                name="unitType"
+                placeholder="Select Unit Type"
+                required
+              >
+                <option selected disabled value="">
+                  Select unit type
+                </option>
+                <option value="kg">Kilograms (kg)</option>
+                <option value="g">Grams (g)</option>
+                <option value="l">Liters (l)</option>
+                <option value="ml">Milliliters (ml)</option>
+                <option value="piece">Pieces</option>
+                <option value="lb">Pounds (lb)</option>
+                <option value="oz">Ounces (oz)</option>
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                Please select a unit type.
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
 
-      <Form.Group className={Styles.formGroup}>
-        <Form.Label className={Styles.label}>
-          SKU
-          <Form.Control
-            name="SKU"
-            type="number"
-            placeholder="Enter SKU"
-            required
-          />
-        </Form.Label>
-
-        <Form.Label className={Styles.label}>
-          Last Ordered
-          <Form.Control
-            name="LastOrdered"
-            type="date"
-            placeholder="Enter Last Ordered"
-            required
-          />
-        </Form.Label>
+      <Form.Group>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">Category </Form.Label>
+              <Form.Control
+                name="Category"
+                type="text"
+                placeholder="Enter Category"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a category.
+              </Form.Control.Feedback>
+            </Col>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">Supplier </Form.Label>
+              <Form.Control
+                name="Supplier"
+                type="text"
+                placeholder="Enter Supplier"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a supplier.
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
       </Form.Group>
 
-      <ButtonGroup className={Styles.btnGroup}>
-        <Button
-          type="button"
-          className={Styles.cancelBtn}
-          onClick={handleClose}
-        >
+      <Form.Group>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">SKU </Form.Label>
+              <Form.Control
+                name="SKU"
+                type="number"
+                placeholder="Enter SKU"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a SKU number.
+              </Form.Control.Feedback>
+            </Col>
+            <Col>
+              <Form.Label className="text-muted h6 mt-3">
+                Last Ordered
+              </Form.Label>
+              <Form.Control
+                name="LastOrdered"
+                type="date"
+                placeholder="Enter Last Ordered"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a last ordered date.
+              </Form.Control.Feedback>
+            </Col>
+          </Row>
+        </Container>
+      </Form.Group>
+
+      <ButtonGroup className="mt-3 d-flex gap-3 p-2">
+        <Button type="button" variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
-        <Button type="submit" className={Styles.submitBtn}>
+
+        <Button type="submit" variant="primary">
           Save
         </Button>
       </ButtonGroup>
