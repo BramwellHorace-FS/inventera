c8b9d1c12c5d8f84ee019f458eab05a77b88e077
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-20 16:55:03 -0400

    added prop types to sidebar

--- reactjs/client/src/components/sidebar/index.js
+++ reactjs/client/src/components/sidebar/index.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { BsXLg } from 'react-icons/bs';
 import { FiMenu } from 'react-icons/fi';
+import { Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 
-export default function Sidebar() {
+export default function Sidebar({ children }) {
   const [show, setShow] = useState(false);
 
   const toggleShow = () => {
@@ -19,10 +21,12 @@ export default function Sidebar() {
         }`}
       >
         <BsXLg className={styles.close} onClick={toggleShow} />
-        <div>
-          <p>Hey</p>
-        </div>
+        <Container>{children}</Container>
       </aside>
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  children: PropTypes.node.isRequired,
+};
