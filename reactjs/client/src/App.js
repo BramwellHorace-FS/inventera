fe83743c1af6a910de58580c24c3328b3878bd99
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-20 17:15:50 -0400

    added logo to sidebar

--- reactjs/client/src/App.js
+++ reactjs/client/src/App.js
@@ -1,3 +1,10 @@
+import Sidebar from './components/sidebar';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+      <p>Content</p>
+    </div>
+  );
 }
