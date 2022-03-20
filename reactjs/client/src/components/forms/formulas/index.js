d51eeff29160cf68255307579bcfb99c54c468ed
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-20 15:15:27 -0400

    initial commit

---
+++ reactjs/client/src/components/forms/formulas/index.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import {
+  Form,
+  Button,
+  ButtonGroup,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+export default function FormulaForm({ handleClose }) {
+  return <div>index</div>;
+}
+
+FormulaForm.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+};
