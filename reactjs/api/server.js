525db76fbcabef86778bbbed81f6bb4c67056da3
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-21 03:17:02 -0400

    updated server with heroku settings

--- reactjs/api/server.js
+++ reactjs/api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -15,5 +16,15 @@ app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
