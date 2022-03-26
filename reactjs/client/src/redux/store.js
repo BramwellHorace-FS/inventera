599b206d2aa67a1c3a831aef7d554496133ebf09
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-26 14:16:47 -0400

    added formulas slice to store

--- reactjs/client/src/redux/store.js
+++ reactjs/client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import materialsReducer from './features/materials/materialsSlice';
 import productsReducer from './features/products/productsSlice';
+import formulasReducer from './features/formulas/formulasSlice';
 
 export const store = configureStore({
   reducer: {
     materials: materialsReducer,
     products: productsReducer,
+    formulas: formulasReducer,
   },
 });
