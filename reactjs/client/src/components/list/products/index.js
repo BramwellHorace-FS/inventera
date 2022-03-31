721368e50314a2500e921dfe2cb6985c382b696b
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-03-31 12:29:03 -0400

    Added delete and edit buttons for checked items

--- reactjs/client/src/components/list/products/index.js
+++ reactjs/client/src/components/list/products/index.js
@@ -1,56 +1,85 @@
-import React from 'react';
-import { Form, Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Table, ButtonGroup, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 export default function ProductList({ products, status }) {
+  const [productsChecked, setProductsChecked] = useState([]);
+
+  const handleCheck = (e, id) => {
+    const newProductsChecked = [...productsChecked];
+
+    if (e.target.checked) {
+      newProductsChecked.push(id);
+    } else {
+      newProductsChecked.splice(newProductsChecked.indexOf(id), 1);
+    }
+
+    setProductsChecked(newProductsChecked);
+  };
+
+  console.log(productsChecked);
+
   return (
-    <Table responsive className="noWrap">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>StockLevel</th>
-          <th>MinLevel</th>
-          <th>Unit Cost</th>
-          <th>SKU</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {status === 'loading' && (
+    <>
+      {productsChecked.length > 0 && (
+        <ButtonGroup>
+          <Button variant="danger"> Delete </Button>
+          {productsChecked.length === 1 && (
+            <Button variant="outline-dark">Edit</Button>
+          )}
+        </ButtonGroup>
+      )}
+      <Table responsive className="noWrap">
+        <thead>
           <tr>
-            <td colSpan="8">Loading...</td>
+            <th>Name</th>
+            <th>StockLevel</th>
+            <th>MinLevel</th>
+            <th>Unit Cost</th>
+            <th>SKU</th>
+            <th>Category</th>
           </tr>
-        )}
-        {status === 'error' && (
-          <tr>
-            <td colSpan="8">Error...</td>
-          </tr>
-        )}
-        {status === 'success' && (
-          <>
-            {products.map((product) => (
-              <tr key={product.id}>
-                <td>
-                  <Form.Check type="checkbox">
-                    <Form.Check.Input type="checkbox" />
-                    <Form.Check.Label>{product.name}</Form.Check.Label>
-                  </Form.Check>
-                </td>
-                <td>
-                  {product.stockLevel} {product.unitType}
-                </td>
-                <td>
-                  {product.minLevel} {product.unitType}
-                </td>
-                <td>${product.unitPrice}</td>
-                <td>{product.sku}</td>
-                <td>{product.category}</td>
-              </tr>
-            ))}
-          </>
-        )}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {status === 'loading' && (
+            <tr>
+              <td colSpan="8">Loading...</td>
+            </tr>
+          )}
+          {status === 'error' && (
+            <tr>
+              <td colSpan="8">Error...</td>
+            </tr>
+          )}
+          {status === 'success' && (
+            <>
+              {products.map((product) => (
+                <tr key={product.id}>
+                  <td>
+                    <Form.Check type="checkbox">
+                      <Form.Check.Input
+                        type="checkbox"
+                        onChange={(e) => handleCheck(e, product.id)}
+                      />
+                      <Form.Check.Label>{product.name}</Form.Check.Label>
+                    </Form.Check>
+                  </td>
+                  <td>
+                    {product.stockLevel} {product.unitType}
+                  </td>
+                  <td>
+                    {product.minLevel} {product.unitType}
+                  </td>
+                  <td>${product.unitPrice}</td>
+                  <td>{product.sku}</td>
+                  <td>{product.category}</td>
+                </tr>
+              ))}
+            </>
+          )}
+        </tbody>
+      </Table>
+    </>
   );
 }
 
