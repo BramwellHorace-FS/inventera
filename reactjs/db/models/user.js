40623bfd69acd7a7db26481a89eecfd54120da96
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-04-07 17:52:01 -0400

    updated unit model. added seed data and updated model validation and model association

--- reactjs/db/models/user.js
+++ reactjs/db/models/user.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Unit, {
+        foreignKey: 'userId',
+        as: 'units',
+      });
     }
   }
   User.init(
