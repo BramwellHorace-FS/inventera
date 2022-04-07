745834a296dc6bef9309b576525f5b639b1e08ba
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-04-07 15:40:30 -0400

    created user model and seed data

---
+++ reactjs/db/migrations/20220407192943-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        unique: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        validate: {
+          min: 8,
+        },
+      },
+      businessName: {
+        type: Sequelize.STRING,
+        validate: {
+          max: 50,
+        },
+      },
+      website: {
+        type: Sequelize.STRING,
+        validate: {
+          max: 50,
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
