522c3a7d9c02a933e49807bc18e0f0b28974cdd5
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-04-08 16:46:17 -0400

    initial commit

---
+++ reactjs/db/models/material.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Material extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Material.init({
+    name: DataTypes.STRING,
+    stock: DataTypes.DECIMAL,
+    minStock: DataTypes.DECIMAL,
+    unitId: DataTypes.UUID,
+    unitCost: DataTypes.DECIMAL,
+    sku: DataTypes.STRING,
+    categoryId: DataTypes.UUID,
+    supplierId: DataTypes.UUID,
+    lastOrdered: DataTypes.DATE,
+    userId: DataTypes.UUID
+  }, {
+    sequelize,
+    modelName: 'Material',
+  });
+  return Material;
+};
