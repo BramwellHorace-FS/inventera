450f951769ac3b2a715095fccc5626bebd6ba46a
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-04-17 13:43:41 -0400

    completed test for production board route

---
+++ reactjs/app/__test__/productionBoard.test.js
@@ -0,0 +1,13 @@
+const app = require('../');
+const supertest = require('supertest');
+const request = supertest(app);
+
+// GET /api/boards
+describe('GET /api/boards', () => {
+  it('should return a 200 status code and an array of production boards', async () => {
+    const response = await request.get('/api/boards');
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
