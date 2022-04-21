0fdf2046e339a81dcb0dc098866f9fbcc80b9d14
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-04-21 09:21:14 -0400

    initial commit

--- reactjs/app/controllers/user.js
+++ reactjs/app/controllers/user.js
@@ -2,43 +2,65 @@ const { User } = require('../../db/models');
 const { uuid } = require('uuidv4');
 const bcrypt = require('bcrypt');
 const { CustomError } = require('../utils/errors');
+const { cloudinary } = require('../utils/cloudinary');
 
-// GET /api/user
+// GET /api/user - returns single user object
 exports.getOne = async (req, res, next) => {
   try {
-    const id = req.user.id;
+    const user = await User.findByPk(req.user.id);
 
-    const user = await User.findByPk(id);
+    if (!user) {
+      throw new CustomError('UserNotFoundError', 404, 'User not found');
+    }
 
-    if (!user) throw new CustomError('NotFoundError', 404, `No user with id ${id}`);
-
-    res.status(200).json({ user });
-  } catch (err) {
-    next(err);
-  }
-};
-
-// PUT /api/user/:id
-exports.update = async (req, res, next) => {
-  try {
-    const id = req.user.id;
-
-    if (!id) throw new CustomError('NotFoundError', 404, `No user with id ${id}`);
-
-    const user = await User.findByPk(id);
-    const { email, password, name, businessName, website } = req.body;
-    const salt = await bcrypt.genSalt(10);
-
-    await user.update({
-      name: name || user.name,
-      email: email || user.email,
-      password: password ? await bcrypt.hash(password, salt) : user.password,
-      businessName: businessName || user.businessName,
-      website: website || user.website,
+    res.status(200).json({
+      status: 'success',
+      message: 'User retrieved successfully',
+      user,
     });
-
-    res.status(200).json({ user });
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    next(error);
   }
 };
+
+// PUT /api/user - updates user object
+
+// // GET /api/user
+// exports.getOne = async (req, res, next) => {
+//   try {
+//     const id = req.user.id;
+
+//     const user = await User.findByPk(id);
+
+//     if (!user) throw new CustomError('NotFoundError', 404, `No user with id ${id}`);
+
+//     res.status(200).json({ user });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// // PUT /api/user/:id
+// exports.update = async (req, res, next) => {
+//   try {
+//     const id = req.user.id;
+
+//     if (!id) throw new CustomError('NotFoundError', 404, `No user with id ${id}`);
+
+//     const user = await User.findByPk(id);
+//     const { email, password, name, businessName, website } = req.body;
+//     const salt = await bcrypt.genSalt(10);
+
+//     await user.update({
+//       name: name || user.name,
+//       email: email || user.email,
+//       password: password ? await bcrypt.hash(password, salt) : user.password,
+//       businessName: businessName || user.businessName,
+//       website: website || user.website,
+//     });
+
+//     res.status(200).json({ user });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
