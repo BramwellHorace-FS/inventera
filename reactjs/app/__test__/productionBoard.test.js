5e06197a34db49a778a6132f5353572cf629933b
BramwellHorace-FS <hbramwell@student.fullsail.edu>
2022-04-21 13:26:28 -0400

    updated production board test

--- reactjs/app/__test__/productionBoard.test.js
+++ reactjs/app/__test__/productionBoard.test.js
@@ -2,18 +2,16 @@ const app = require('../');
 const supertest = require('supertest');
 const request = supertest(app);
 
-describe('must pass', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+// GET /api/boards
+describe('GET /api/boards', () => {
+  it('should return a status code of 200, a status of success, message and array of boards', async () => {
+    const response = await request.get('/api/boards');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('status');
+    expect(response.body.status).toBe('success');
+    expect(response.body).toHaveProperty('message');
+    expect(response.body).toHaveProperty('boards');
+    expect(Array.isArray(response.body.boards)).toBe(true);
   });
 });
-
-// GET /api/boards
-// describe('GET /api/boards', () => {
-//   it('should return a 200 status code and an array of production boards', async () => {
-//     const response = await request.get('/api/boards');
-
-//     expect(response.status).toBe(200);
-//     expect(Array.isArray(response.body)).toBe(true);
-//   });
-// });
